a477ed12d20986179f23e42652d62a87dae55989
Loh Wee Renn <lohwr-wm24@student.tarc.edu.my>
2026-08-19 03:17:15 +0800

    Fix unresolved merge conflict markers in migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,11 +5,7 @@ module.exports = async function (deployer) {
     await deployer.deploy(ReputationToken);
     const token = await ReputationToken.deployed();
     await deployer.deploy(Escrow, token.address);
-<<<<<<< HEAD
     const escrow = await Escrow.deployed();
-
     // Set Escrow contract as the authorized minter on ReputationToken
     await token.setMinter(escrow.address);
-=======
->>>>>>> c56233ca36b00172efd8b50b0439a7375176d7e4
 };
